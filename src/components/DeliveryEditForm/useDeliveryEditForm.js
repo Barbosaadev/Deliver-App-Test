488610c97010d5024da56590b2bed40694a35643
Barbosaadev <silvag1212@gmail.com>
2022-12-12 17:42:36 -0300

    custom hook for Formik and validation using Yup dependencie

---
+++ src/components/DeliveryEditForm/useDeliveryEditForm.js
@@ -0,0 +1,27 @@
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+
+const Schema = Yup.object({
+  title: Yup.string().required('title is required'),
+  description: Yup.string().required('description is required'),
+  date: Yup.string().required('date is required'),
+});
+
+export const useDeliveryEditForm = ({onSubmit}) => {
+  return useFormik({
+    initialValues: {
+      id: '',
+      title: '',
+      description: '',
+      date: '',
+      toggle: false,
+    },
+
+    onSubmit,
+    validationSchema: Schema,
+    validateOnBlur: true,
+    validateOnChange: false
+  });
+};
+
+export default useDeliveryEditForm
