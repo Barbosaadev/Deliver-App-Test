1c35a5ac619d9ca5368c110e4e5d0253ff2b0d6e
Barbosaadev <silvag1212@gmail.com>
2022-12-12 17:16:21 -0300

    Navigation component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+  return (
+    <nav className="list-none hidden ml-auto space-x-1 sm:flex">
+      <li>
+        <Link
+          to="/"
+          className="px-8 py-3 text-indigo-100 rounded-md  flex bg-indigo-800/50"
+        >
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link
+          to="/add"
+          className="px-8 py-3 text-indigo-100 rounded-md  flex bg-indigo-800/50"
+        >
+          Add Deliveries
+        </Link>
+      </li>
+    </nav>
+  );
+};
+
+export default Nav
