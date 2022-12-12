0950b8d4990478f13cb3f2e975f80f13d9a34a85
Barbosaadev <silvag1212@gmail.com>
2022-12-12 17:39:43 -0300

    Field component for Form

---
+++ src/components/Field.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Field = ({
+        label,
+        errorMessage,
+        invalid,
+        component: Component,
+        ...rest
+    }) => {
+        return (
+        <label className="block">
+            {label && <span className="block text-sm mb-2">{label}</span>}
+            {Component && Component({ invalid, ...rest })}
+            {invalid && <span className="text-xs text-red-500">{errorMessage}</span>}
+        </label>
+        );
+    };
+
+export default Field
